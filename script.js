ac0bd962fde1ae4b4b341ebd516f2a62f10361d8
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-01-03 12:35:12 +0100

    aggiunta funzione all uscita del mouse dal bottone

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 
- let button = document.querySelector('#submit');
- let review = document.querySelectorAll('.container > p > span');
+let button = document.querySelector('#submit');
+let review = document.querySelectorAll('.container > p > span');
 
 function passaggio() {
     console.log('sei passato sul tasto');
-     this.style.background = `hsl(0, 0%, 100%)`;
-     this.style.color = `hsl(25, 97%, 53%)`;
+    this.style.background = `hsl(0, 0%, 100%)`;
+    this.style.color = `hsl(25, 97%, 53%)`;
+}
+function uscita() {
+    console.log('sei uscito dal bottone');
+    this.style.background = '#FC7614';
+    this.style.color = '';
 }
 
-review.forEach(function(review) {
-    review.addEventListener('mouseover', passaggio);
+review.forEach(function (review) {
+    review.addEventListener('mouseover', () => {
+        review.style.background = `hsl(25, 97%, 53%)`;
+        review.style.color = `hsl(0, 0%, 100%)`;
+    });
+    review.addEventListener('mouseout', () => {
+        console.log('sei uscito');
+        review.style.background = '#262E38'
+        review.style.color = '#7C8798'
+    })
 });
 
 // review.addEventListener('mouseover', passaggio);
 button.addEventListener('mouseover', passaggio);
+button.addEventListener('mouseout', uscita);

b78857c570bce0b30d418db591765e03ee09cb31
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-01-09 11:16:53 +0100

    animazione di uscita aggiunta corretamente

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 let button = document.querySelector('#submit');
 let review = document.querySelectorAll('.container > p > span');
+let form = document.querySelector('#form-review');
 
 button.addEventListener('mouseover', entrata);
 button.addEventListener('mouseout', uscita);
@@ -22,4 +23,32 @@ review.forEach( (review) => {
     })
 })
 
+button.addEventListener('click', function() {
+    form.classList.add('active');
+})
+
+
+// ---------------------------------------------------------------------------------------------------------
+
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click', () => {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', () => {
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
+});
 
